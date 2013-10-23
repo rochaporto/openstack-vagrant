3784d3744cfd2601c16f2324a23c87c289504454
Ricardo Rocha <ricardo@catalyst.net.nz>
2013-10-23 14:35:18 +1300

    sample with keystone config in controller.

--- puppet/manifests/controller.pp
+++ puppet/manifests/controller.pp
@@ -1,3 +1,19 @@
 import "default.pp"
 
+class { "mysql::server": }
 
+#
+# Keystone setup and configuration 
+#
+class { "keystone::db::mysql":
+	password			=> "${default_password}",
+	allowed_hosts	=> ["localhost", "%"],
+}
+
+class { "keystone":
+	verbose					=> true,
+	catalog_type		=> "sql",
+	admin_token			=> "${default_token}",
+	sql_connection	=> "mysql://keystone:${default_password}@localhost/keystone",
+}
+	
--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -7,3 +7,11 @@ File { owner => 0, group => 0, mode => 0644, }
 file { "/etc/motd": 
 	content => "Vagrant managed by puppet... it works!",
 }
+
+exec { "apt-update": command => "/usr/bin/apt-get update", }
+Exec["apt-update"] -> Package <| |>
+
+$default_password = "123456"
+$default_email		= "ricardo@catalyst.net.nz"
+$default_token		= "e09884cfa5d10e938b02"
+
