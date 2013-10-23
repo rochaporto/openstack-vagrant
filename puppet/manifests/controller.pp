b7beac8b458aa106e82cadaa61d8f5dae92a01c1
Ricardo Rocha <ricardo@catalyst.net.nz>
2013-10-23 17:27:50 +1300

    tab sanity!

--- puppet/manifests/controller.pp
+++ puppet/manifests/controller.pp
@@ -6,14 +6,14 @@ class { "mysql::server": }
 # Keystone setup and configuration 
 #
 class { "keystone::db::mysql":
-	password			=> "${default_password}",
+	password	=> "${default_password}",
 	allowed_hosts	=> ["localhost", "%"],
 }
 
 class { "keystone":
-	verbose					=> true,
-	catalog_type		=> "sql",
-	admin_token			=> "${default_token}",
+	verbose		=> true,
+	catalog_type	=> "sql",
+	admin_token	=> "${default_token}",
 	sql_connection	=> "mysql://keystone:${default_password}@localhost/keystone",
 }
 	
@@ -22,30 +22,30 @@ class { "keystone":
 #
 
 class { "glance::db::mysql":
-	password			=> "${default_password}",
+	password	=> "${default_password}",
 	allowed_hosts	=> ["localhost", "%"],
 }
 
 class { "glance::api":
-	verbose						=> true,
+	verbose			=> true,
 	keystone_password	=> "${default_password}",
 	sql_connection		=> "mysql://glance:${default_password}@localhost/glance",
 }
 
 class { "glance::registry":
-	verbose						=> true,
+	verbose			=> true,
 	keystone_tenant		=> "service",
-	keystone_user			=> "glance",
+	keystone_user		=> "glance",
 	keystone_password	=> "${default_password}",
 	sql_connection		=> "mysql://glance:${default_password}@localhost/glance",
 }
 
 class { "glance::keystone::auth":
-	password					=> "${default_password}",
-	email							=> "${default_email}",
+	password		=> "${default_password}",
+	email			=> "${default_email}",
 	public_address		=> $ipaddress,
-	admin_address			=> $ipaddress,
+	admin_address		=> $ipaddress,
 	internal_address	=> $ipaddress,
-	region						=> "${default_region}",
+	region			=> "${default_region}",
 }
 
--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -8,7 +8,7 @@ file { "/etc/motd":
 	content => "Vagrant managed by puppet... it works!",
 }
 
-$default_password = "123456"
+$default_password 	= "123456"
 $default_email		= "admin@example.com"
 $default_token		= "e09884cfa5d10e938b02"
 $default_region		= "1.example.com"
@@ -16,9 +16,9 @@ $default_region		= "1.example.com"
 class { "apt": }
 
 apt::source { "cloudarchive-havana":
-	location					=> "http://ubuntu-cloud.archive.canonical.com/ubuntu",
-	release						=> "precise-updates/havana",
-	repos							=> "main",
+	location		=> "http://ubuntu-cloud.archive.canonical.com/ubuntu",
+	release			=> "precise-updates/havana",
+	repos			=> "main",
 	required_packages	=> "ubuntu-cloud-keyring",
 }
 
