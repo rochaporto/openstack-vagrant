12d419171e04b641478c996472ae04c111ae13ba
Ricardo Rocha <ricardo@catalyst.net.nz>
2013-10-23 17:35:10 +1300

    more tab sanity

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -4,10 +4,10 @@ File { owner => 0, group => 0, mode => 0644, }
 
 file { "/etc/motd": content => "Vagrant managed by puppet... it works!", }
 
-$default_password 	= "123456"
-$default_email		= "admin@example.com"
-$default_token		= "e09884cfa5d10e938b02"
-$default_region		= "1.example.com"
+$default_password = "123456"
+$default_email    = "admin@example.com"
+$default_token    = "e09884cfa5d10e938b02"
+$default_region   = "1.example.com"
 
 class { "apt": }
 
